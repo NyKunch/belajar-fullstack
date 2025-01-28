f037541f9c4d20ec507dd30ba895be3fabe88559
NyKunch <ngakanputudoni@gmail.com>
2025-01-28 17:39:05 +0800

    Fix some bug from the /add endpoint

--- 8 Postgres Travel Tracker/README.md
+++ 8 Postgres Travel Tracker/README.md
@@ -3,3 +3,12 @@
 This project main idea is to highlight the countries that have been visited. 
 
 Using postgres to retrieve the database and map through the database to get country_code. 
+
+Application Feature:
+- Show every country that had been visited.
+- Add new country using the input text section.
+
+
+Update: 
+- Fixing minor bug when user type the wrong country name by returning error to the form's placeholder.
+- Using wildcard to match the user input with the database's country name on database querying.
--- 8 Postgres Travel Tracker/index.js
+++ 8 Postgres Travel Tracker/index.js
@@ -4,6 +4,7 @@ import pg from "pg";
 
 const app = express();
 const port = 3000;
+// let country_codes = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -18,18 +19,50 @@ const db = new pg.Client({
 
 db.connect();
 
-app.get("/", async (req, res) => {
+async function checkVisited() {
   const result = await db.query("SELECT * FROM visited_countries");
   const country_codes = result.rows.map((country) => country.country_code);
-  res.render("index.ejs", {total: result.rows.length, countries: country_codes});
+  return country_codes;
+}
+
+app.get("/", async (req, res) => {
+  const country_codes = await checkVisited();
+  res.render("index.ejs", {
+    total: country_codes.length, 
+    countries: country_codes
+  });
 });
 
 app.post("/add", async (req, res) => {
-  const countryCode = await db.query("SELECT * FROM countries WHERE country_name = $1", [req.body.country]);
-  if(countryCode.rows[0].length != 0){
-    await db.query("INSERT INTO visited_countries(country_code) VALUES($1)", [countryCode.rows[0].country_code]);
-    res.redirect("/");
+  try {
+    const result = await db.query(
+      "SELECT * FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [req.body.country.toLowerCase()]
+      );
+    const country_code = result.rows[0].country_code
+    try {
+      await db.query(
+        "INSERT INTO visited_countries(country_code) VALUES($1);",
+        [country_code]
+        );
+      res.redirect("/");
+    } catch (error) {
+      const country_codes = await checkVisited();
+      res.render("index.ejs", {
+        total: country_codes.length, 
+        countries: country_codes, 
+        error: "Country already added, try again"
+      });
+    }
+  } catch (error) {
+    const country_codes = await checkVisited();
+    res.render("index.ejs", {
+      total: country_codes.length, 
+      countries: country_codes, 
+      error: "Can't find that country, try again"
+    });
   }
+
 });
 
 app.listen(port, () => {
