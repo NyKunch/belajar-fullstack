af72c55dff55937482d6d88f92004dcc848c7bab
NyKunch <ngakanputudoni@gmail.com>
2025-02-07 14:16:15 +0800

    Update: styling react component using inline css

--- 12 Pre-React Practice/test-react-app/src/main.jsx
+++ 12 Pre-React Practice/test-react-app/src/main.jsx
@@ -1,34 +1,50 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
 
+// function MyButton(){
+//   const [randomNumber, setRandomNumber] = React.useState(0)
+//   return (
+//     <>
+//       <h3>Tekan untuk mendapatkan nomor acak</h3>
+//       <button onClick={() => {setRandomNumber(Math.floor(Math.random()*100))}}>
+//       {randomNumber}
+//     </button>
+//     </>
+//   )
+// }
+
 // createRoot(document.getElementById('root')).render(
 //   <StrictMode>
-//     <App />
-//   </StrictMode>,
+//     <h1>Halo Dunia</h1>
+//     <MyButton />
+//   </StrictMode>
 // )
 
 import React from "react"
 import { createRoot } from 'react-dom/client'
-import { useState } from "react"
+// import { useState } from "react"
 
+const hour = new Date().getHours()
+const time = hour <= 4 ? 'Dawn' 
+  : hour <= 10 ? 'Morning' 
+  : hour <= 15 ? 'Afternoon' 
+  : hour <= 20 ? 'Evening'
+  : 'Night'
 
-function MyButton(){
-  const [randomNumber, setRandomNumber] = useState(0)
-  return (
-    <>
-      <h3>Tekan untuk mendapatkan nomor acak</h3>
-      <button onClick={() => {setRandomNumber(Math.floor(Math.random()*100))}}>
-      {randomNumber}
-    </button>
-    </>
-  )
+const timeColor = hour <= 4 ? 'cyan' 
+: hour <= 10 ? 'red' 
+: hour <= 15 ? 'green' 
+: hour <= 20 ? 'purple'
+: 'blue'
+
+const fontColor = {
+  color: timeColor
 }
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <h1>Halo Dunia</h1>
-    <MyButton />
-  </StrictMode>
+  <>
+    <h1 style={fontColor}>Good {time}</h1>
+  </>
 )
