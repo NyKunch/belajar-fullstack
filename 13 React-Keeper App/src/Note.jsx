8d77b089d3c8550518d7c3af0fb9f2c3f44aa063
NyKunch <ngakanputudoni@gmail.com>
2025-02-07 23:06:13 +0800

    Initial commit

---
+++ 13 React-Keeper App/src/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Note() {
+    return (
+        <>
+            <div className="note">
+                <h1>
+                    This is a title
+                </h1>
+                <p>
+                    This is a content
+                </p>
+            </div>
+        </>
+    )
+}
