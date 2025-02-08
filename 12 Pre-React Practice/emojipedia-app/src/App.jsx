bb91dcd446f807bd48a6f682f07682017c77fafe
NyKunch <ngakanputudoni@gmail.com>
2025-02-08 22:02:56 +0800

    Complete the code

--- 12 Pre-React Practice/emojipedia-app/src/App.jsx
+++ 12 Pre-React Practice/emojipedia-app/src/App.jsx
@@ -1,2 +1,28 @@
-import React from "react"
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
 
+function mapEmoji(emot) {
+  return (
+    <Entry
+      key={emot.id}
+      emoji={emot.emoji}
+      name={emot.name}
+      meaning={emot.meaning}
+    />
+  )
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+      {emojipedia.map(mapEmoji)}
+      </dl>
+    </div>
+  );
+}
