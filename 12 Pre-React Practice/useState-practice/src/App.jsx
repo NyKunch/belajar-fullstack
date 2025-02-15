ad846169f4c15f22ffd4e6b6814857c885e9e1b3
NyKunch <ngakanputudoni@gmail.com>
2025-02-15 21:44:49 +0800

    Practicing React Form

--- 12 Pre-React Practice/useState-practice/src/App.jsx
+++ 12 Pre-React Practice/useState-practice/src/App.jsx
@@ -1,19 +1,56 @@
 import React from "react"
 
 export default function App() {
-  const [time, setTime] = React.useState(new Date().toLocaleTimeString())
+  const [name, setName] = React.useState("")
+  const [headingName, setHeadingName] = React.useState()
 
-  function updateTime(){
-    setTime(new Date().toLocaleTimeString())
+  function changeName(event) {
+    setName(event.target.value)
   }
 
-  setInterval(updateTime, 1000)
+  function changeHeadingName(event) {
+    setHeadingName(name)
+    event.preventDefault()
+  }
 
   return (
-    <>
+    <div className="container">
       <h1>
-        {time}
+        Hello {headingName}
       </h1>
-    </>
+      <form
+        onSubmit={changeHeadingName}
+      >
+        <input 
+          type="text" 
+          placeholder="What's your name?" 
+          onChange={changeName}
+        />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
   )
 }
+
+
+
+
+// export default function App() {
+//   const [time, setTime] = React.useState(new Date().toLocaleTimeString())
+
+//   function updateTime(){
+//     setTime(new Date().toLocaleTimeString())
+//   }
+
+//   setInterval(updateTime, 1000)
+
+//   return (
+//     <>
+//       <h1>
+//         {time}
+//       </h1>
+//     </>
+//   )
+// }
