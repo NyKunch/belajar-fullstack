b1318b79a775983d93b35935c776c429a6baf049
NyKunch <ngakanputudoni@gmail.com>
2025-03-01 15:01:47 +0800

    Initial Commit

---
+++ 14 React Project/memory-card-game/src/App.jsx
@@ -0,0 +1,11 @@
+import CardSection from "./components/CardSection"
+
+const App = () => {
+  return (
+    <div className='container'>
+      <CardSection />
+    </div>
+  )
+}
+
+export default App
