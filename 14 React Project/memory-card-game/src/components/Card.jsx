ab5f2993a3e3680676e7779253384b3d7a90c2e7
NyKunch <ngakanputudoni@gmail.com>
2025-03-01 15:01:11 +0800

    Initial Commit

---
+++ 14 React Project/memory-card-game/src/components/Card.jsx
@@ -0,0 +1,12 @@
+const Card = () => {
+    return (
+        <div className='card'>
+            <img src='https://picsum.photos/300/400' alt='Random Img'/>
+            <h3>
+                Nama Pokemon
+            </h3>
+        </div>
+    )
+}
+
+export default Card
---
+++ 14 React Project/memory-card-game/src/components/CardSection.jsx
@@ -0,0 +1,22 @@
+import Card from "./Card"
+
+const CardSection = () => {
+    return (
+        <div className="card-container">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
+
+export default CardSection
